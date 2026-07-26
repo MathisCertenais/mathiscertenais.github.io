a5dda0eb4a68176de57e3b4ab0b75ee544cb2a31
mathiscertenais <mathis.certenais@irisa.fr>
2026-07-26 21:23:48 +0200

    public directory config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,13 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   build: {
+    outDir: 'dist',
+    emptyOutDir: true,
     cssMinify: false,
-  },
-})
+    assetsDir: 'assets',
+    chunkSizeWarningLimit: 1000,
+    },
+    publicDir: 'public',
+  });
